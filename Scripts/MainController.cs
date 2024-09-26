5657ed276f6da96d972d9bd9daedf3194c0bef61
KeeganMiller <126998795+KeeganMiller@users.noreply.github.com>
2024-09-26 22:04:46 +1000

    Misc

--- Scripts/MainController.cs
+++ Scripts/MainController.cs
@@ -1,12 +1,13 @@
 ﻿using System.Collections.Generic;
 using Godot;
 using GameDevIncEditor;
+using Newtonsoft.Json;
 
 namespace GameDevIncEditor.Godot;
 
 public partial class MainController : Node2D
 {
-    public string ModulesPath { get; private set; } = "res://Data/Data.json";                  // Path to location of the modules file
+    public string ModulesPath { get; private set; } = "C:/Users/KAMil/Documents/gamedevinceditor/Data/Data.json";                  // Path to location of the modules file
 
     public static MainController Instance { get; private set; }
 
@@ -14,5 +15,38 @@ public partial class MainController : Node2D
     {
         base._Ready();
         Instance = this; 
+    }
+
+    public override void _Process(double delta)
+    {
+        base._Process(delta);
+        if(Input.IsActionJustPressed("TestKey"))
+        {
+            var module = new BaseModule
+            {
+                Id = 0,
+                ModuleName = "Hello, World",
+                ModuleJobType = 0,
+                IconPath = "",
+                MinAAA = 82,
+                MaxAAA = 94,
+                MinIII = 13,
+                MaxIII = 45,
+                MinIndie = 13,
+                MaxIndie = 20
+            };
+
+            Loader.Instance.Data.Add(module);
+            switch(Writer.Instance.WriteToFile())
+            {
+                case EWriteToFileResult.Success:
+                    GD.Print("Successfully added module");
+                    break;
+                case EWriteToFileResult.FileNotFound:
+                    GD.Print("Could not find location to file");
+                    break;
+
+            }
+        }
     }
 }
--- Scripts/Writer.cs
+++ Scripts/Writer.cs
@@ -1,13 +1,49 @@
 ﻿using System.Collections.Generic;
 using GameDevIncEditor;
 using Godot;
+using Newtonsoft.Json;
+using System.IO;
+using System;
 
 namespace GameDevIncEditor.Godot;
 
+public enum EWriteToFileResult
+{
+    Success,
+    FileNotFound,
+    UnknownError
+}
+
 public partial class Writer : Node
 {
-    public void WriteToFile()
+    public static Writer Instance { get; private set; }
+    public event Action WriteFileEvent;
+    public override void _Ready()
     {
+        base._Ready();
+        Instance = this;
+    }
+    public EWriteToFileResult WriteToFile()
+    {
+        var data = Loader.Instance.Data;
+        string updatedData = "[";
+        foreach(var item in data)
+        {
+            var jsonData = JsonConvert.SerializeObject(item);
+            updatedData += jsonData + ",";
+        }
 
+        updatedData += "]";
+
+        if(File.Exists(MainController.Instance.ModulesPath))
+        {
+            File.WriteAllText(MainController.Instance.ModulesPath, updatedData);
+            WriteFileEvent?.Invoke();
+            return EWriteToFileResult.Success;
+        } else
+        {
+            GD.PushWarning("Path to file not found");
+            return EWriteToFileResult.FileNotFound;
+        }
     }
 }
