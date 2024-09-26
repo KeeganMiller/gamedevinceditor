d167d328bf57a52c66e9ec55b1f8ba2bbb200e4e
KeeganMiller <126998795+KeeganMiller@users.noreply.github.com>
2024-09-27 01:22:17 +1000

    Setup the button with skeleton

--- Scripts/Buttons/ModuleButton.cs
+++ Scripts/Buttons/ModuleButton.cs
@@ -1,12 +1,23 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
+using Godot;
+using GameDevIncEditor;
 
-namespace GameDevIncEditor.Scripts.Buttons
+namespace GameDevIncEditor.Godot;
+
+public partial class ModuleButton : Button
 {
-    internal class ModuleButton
+    public BaseModule ModuleRef;
+
+    public override void _Ready()
     {
+        base._Ready();
+        Connect("pressed", new Callable(this, "SelectModule"));
+    }
+
+    private void SelectModule()
+    {
+        // TODO: Open Module
+        // TODO: Open Module Pop Up
+        // TODO: Setup the module details
     }
 }
